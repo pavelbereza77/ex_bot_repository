e36d81586cc21aea5ae079ea6bf6f5216f8733f2
Pavel Bereza <rabotam2013@yandex.ru>
2024-06-06 11:31:25 +0300

    добовляем2

--- ex7.py
+++ ex7.py
@@ -1,10 +1,20 @@
 import os
 
-import dotenv
+from environs import Env
 
+env = Env()  # Создаем экземпляр класса Env
+env.read_env()  # Методом read_env() читаем файл .env и загружаем из него переменные в окружение
+print(env.read_env())
+bot_token = env('BOT_TOKEN')  # Получаем и сохраняем значение переменной окружения в переменную bot_token
+admin_id = env.int('ADMIN_ID')  # Получаем и преобразуем значение переменной окружения к типу int,
+# затем сохраняем в переменной admin_id
 
-dotenv.load_dotenv()
+# Выводим значения переменных в терминал
+print(bot_token)
+print(admin_id)
+print()
 
+# Убедимся также, что переменные есть в окружении
 print(os.getenv('BOT_TOKEN'))
 print(os.getenv('ADMIN_ID'))
 
